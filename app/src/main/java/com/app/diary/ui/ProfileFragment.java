ce1d7e669304597c99d0c48dfdff5eba5296bb82
LIUYIBO <2602731702@qq.com>
2025-06-03 14:12:44 +0800

    修复个人中心无法跳转bug

--- app/src/main/java/com/app/diary/ui/ProfileFragment.java
+++ app/src/main/java/com/app/diary/ui/ProfileFragment.java
@@ -24,7 +24,7 @@ public class ProfileFragment extends BaseFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_user_profile, container, false);
     }
 
     @Override
